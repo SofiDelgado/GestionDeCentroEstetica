4697ec243eeed3051052cf7c36c9c1a7dae2be9e
SofiDelgado <sofi5_09@hotmail.com>
2022-02-12 17:31:12 -0300

    Update menus.h

--- menus.h
+++ menus.h
@@ -33,7 +33,47 @@ void MenuAdmin(FILE *arch,Profesionales Prof,Recepcionista Recep)
 	}while(op!=6);
 	
 }
-void MenuRecepcion()
+void MenuRecep(FILE *client,Cliente Clien,FILE *prof, Profesionales Prof, FILE* turn, Turnos Turn)
 {
+	int Opc;
 	
+	do
+	{
+		printf("Modulo del recepcionista\n");
+		printf("==========================");
+		printf("\n1.- Registrar Cliente");
+		printf("\n2.- Registrar Turno");
+		printf("\n3.- Listado de Atenciones por Profesional y Fecha\n");
+		printf("\n4.- Cerrar la aplicacion.\n");
+		printf("Ingrese una opcion: _");
+		scanf("%d", &Opc);
+		
+		switch(Opc)
+		{
+			case 1:
+			system("CLS");
+			Regcliente(client,Clien);
+			break;
+	        
+	        case 2:
+	        system("CLS");
+	        Regturno(turn, Turn, prof, Prof);
+	        break;
+	        
+	        case 3:
+	        system("CLS");
+	        Listatenc(turn, Turn, prof, Prof);	
+	        
+	        case 4:
+	        system("CLS");
+	        printf("Saliendo de la aplicacion...");
+	        break;
+	        
+	        default:
+	        printf("Error, ingrese una opcion valida.");
+	        break;
+	        
+		}
+		
+	}while(Opc!=4);
 }
