cbf3fd60423e12dd696676afc6e733c89ed8b795
SofiDelgado <sofi5_09@hotmail.com>
2022-02-13 00:59:09 -0300

    Update Login.h

--- Login.h
+++ Login.h
@@ -1,31 +1,68 @@
-void login(FILE *arch,Profesionales Prof,Recepcionista Recep)
+void login(FILE *prof, FILE *recep,Profesionales Prof,Recepcionista Recep,int &permiso,bool &verificacion)
 {	
 
 	int i,b;
-	arch = fopen ("Profesionales.dat","a+b");
+		int mcontra=0, musuario=0;
+	int d;
 	char usuario[60],contrasenia[60];
-		printf("------------------------");
-		printf("\nINICIO DE SESION\n");
-		printf("\nUSUARIO:");
+	  gotoxy(52,6);
+	printf("INICIO DE SESION");
+		gotoxy(50,11);
+		printf("USUARIO:");
 		_flushall();
 		gets(usuario);
-		printf("\nContrasenia:");
+	gotoxy(50,12);
+		printf("Contrasenia:");
 		_flushall();
 		gets(contrasenia);
 		
-		fread(&Prof,sizeof(Profesionales),1,arch);
+		prof = fopen ("Profesionales.dat","rb");
 		
-		while (!feof(arch))
+		fread(&Prof,sizeof(Profesionales),1,prof);
+		while(!feof(prof) and mcontra==0 and musuario==0)
 		{
-			if (strcmp(usuario, Prof.UsuarioP)==0 and strcmp(contrasenia, Prof.ContraseniaP)==0)
+			if((strcmp(usuario,Prof.UsuarioP)!=0) or (strcmp(contrasenia,Prof.ContraseniaP)!=0)) //Cambio a uso de función strcmp()
 			{
-			b=1;
+				fread(&Prof,sizeof(Profesionales),1,prof);
+			}
+			else
+			{
+				mcontra=1;
+				musuario=1;
+				permiso=Prof.permisoP;
 			}
 		}
-		fclose(arch);
-		    if (b==1)
+		fclose(prof);
+		
+		recep = fopen ("Recepcionistas.dat","rb");
+		fread(&Recep,sizeof(Recepcionista),1,recep);
+		while(!feof(recep) and mcontra==0 and musuario==0)
+		{
+			if((strcmp(usuario,Recep.UsuarioR)!=0) or (strcmp(contrasenia,Recep.ContraseniaR)!=0)) //Cambio a uso de función strcmp()
 			{
-				printf("Correcto!");
-			}else printf("Datos no encontrados...");
-		printf("------------------------");
+				fread(&Recep,sizeof(Recepcionista),1,recep);
+			}
+			else
+			{
+				mcontra=1;
+				musuario=1;
+				permiso=Recep.permisoR;
+			}
+		}
+		
+		
+		fclose(recep);
+		
+		if(mcontra==0 or musuario==0)
+		{
+			gotoxy(45,13);
+			printf("Los datos ingresados son incorrectos");
+		}
+		else
+		{
+			gotoxy(45,13);
+			verificacion=true;
+			printf("Inicio de sesion correcto.");
+		}
+
 }
